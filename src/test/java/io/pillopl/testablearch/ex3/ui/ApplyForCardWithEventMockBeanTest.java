cdcb01debbe49708b946a3fa1ebe047b06458e29
Mohit Pant <MOHITPAN@amdocs.com>
2020-08-09 08:13:32 +0300

    added test using @mockbean but still has disadvantage and does not gaurantees the app will come up in prod even if test pass

--- src/test/java/io/pillopl/testablearch/ex3/ui/ApplyForCardWithEventMockBeanTest.java
+++ src/test/java/io/pillopl/testablearch/ex3/ui/ApplyForCardWithEventMockBeanTest.java
@@ -1,10 +1,17 @@
 package io.pillopl.testablearch.ex3.ui;
 
+import io.pillopl.testablearch.ex3.DomainEventsPublisher;
+import io.pillopl.testablearch.ex3.model.CardApplicationRejected;
+import io.pillopl.testablearch.ex3.model.CardGranted;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.ArgumentMatchers.isA;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,6 +20,13 @@ public class ApplyForCardWithEventMockBeanTest {
     @Autowired
     private CreditCardApplicationController creditCardApplicationController;
 
+    /*
+        the test with this will work successfully but if there no real bean say annotated with @Service of this impl then app will fail even though test will pass
+        (the test is stupid, since the only thing we wanted to test has been mocked out (check docs of @MockBean there is a catch) :D)
+     */
+    @MockBean
+    private DomainEventsPublisher domainEventsPublisher;
+
     @Test
     public void shouldEmitCardGrantedEvent() {
         // when
@@ -20,12 +34,17 @@ public class ApplyForCardWithEventMockBeanTest {
 
         // then (rabbitmq event should be launched , but we dont want our test to depend on some rabbitmq since it might fail in coming up and our test might fail to rather than running in isolation)
 //          // maybe we can mock bean of type DomainEventPublisher
-
+        Mockito.verify(domainEventsPublisher).publish(isA(CardGranted.class));
     }
 
 
     @Test
     public void shouldEmitCardApplicationRejectedEvent() {
+        // when
+        creditCardApplicationController.applyForCard(new CardApplication("50232344"));
 
+        // then (rabbitmq event should be launched , but we dont want our test to depend on some rabbitmq since it might fail in coming up and our test might fail to rather than running in isolation)
+//          // maybe we can mock bean of type DomainEventPublisher
+        Mockito.verify(domainEventsPublisher).publish(isA(CardApplicationRejected.class));
     }
 }
